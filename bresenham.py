696ddc249e428abf69349cd03a2f7b06dfa152f8
Eric <ericv8v9s@outlook.com>
2023-09-29 15:54:53 -0400

    fixed drawing back and down
    The slides say |m| < 1; it actually only works for non-negative m.
    The algorithm also only draws left to right.

--- bresenham.py
+++ bresenham.py
@@ -1,7 +1,7 @@
 from collections.abc import Iterator
 
 
-# the base algo only works for |m| <= 1
+# the base algo only works for 0 <= m <= 1
 def _bresenham_restricted(p1: tuple[int, int], p2: tuple[int, int]) -> \
         Iterator[tuple[int, int]]:
     del_x = p2[0] - p1[0]
@@ -25,14 +25,39 @@ def _bresenham_restricted(p1: tuple[int, int], p2: tuple[int, int]) -> \
 
 def bresenham_line(p1: tuple[int, int], p2: tuple[int, int]) -> \
         list[tuple[int, int]]:
-    del_x = p2[0] - p1[0]
-    del_y = p2[1] - p1[1]
+    # transform input to acceptable m -> compute bresenham -> inverse transform
 
-    if abs(del_y) <= abs(del_x):
-        return list(_bresenham_restricted(p1, p2))
+    # functions to apply to undo the transformations
+    undos = []
 
-    else:
-        # swap x and y for |m| > 1
-        def swap(point: tuple[int, int]) -> tuple[int, int]:
-            return (point[1], point[0])
-        return list(map(swap, _bresenham_restricted(swap(p1), swap(p2))))
+    # have to draw left to right
+    # this does not change the points, only the order; no need to undo
+    if p1[0] > p2[0]:
+        tmp = p1
+        p1 = p2
+        p2 = tmp
+
+    # if drawing downward, flip it up
+    if p2[1] < p1[1]:
+        def flip_y(p):
+            # flips y coordinate w.r.t. p1's y
+            return (p[0], -(p[1] - p1[1]) + p1[1])
+        p2 = flip_y(p2)
+        undos.append(flip_y)  # flip_y is it's own inverse
+
+    # if m is > 1
+    if p2[1] - p1[1] > p2[0] - p1[0]:
+        # swap x and y axes for m > 1
+        def swap(p):
+            return (p[1], p[0])
+        p1 = swap(p1)
+        p2 = swap(p2)
+        undos.append(swap)  # swap is it's own inverse
+
+    # single function to apply all undos
+    def undo_all(point):
+        for f in reversed(undos):
+            point = f(point)
+        return point
+
+    return list(map(undo_all, _bresenham_restricted(p1, p2)))
